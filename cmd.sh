6e364f319fbac9c88d61ddb420b48ce0343c008a
mnauet <malik.akram@gmx.de>
2018-06-16 22:05:48 +0200

    16.06.2018 Jenkins.yml added

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,11 @@ set -e
 if [ "$ENV" = 'DEV']; then
 	echo "Running Development Server"
 	exec python "identidock.py"
+
+elif [ "$ENV" = 'UNIT' ]; then
+	echo "Running unit Testing"
+	exec python tests.py
+
 else
 	echo "Running Production Server"
 	exec  uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
